399e70bef705d1bce37cebb1c1140642f461b1cb
hyomin-dev <choihyomin248@gmail.com>
2025-03-30 04:56:59 +0900

    test

---
+++ src/main/java/com/multi/semiproejct/SemiProejct200okApplication.java
@@ -0,0 +1,13 @@
+package com.multi.semiproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProejct200okApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProejct200okApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/semiproejct/SemiProejct200okApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.semiproejct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProejct200okApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
