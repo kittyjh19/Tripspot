a709b17398a370335442096a4ac5151b59118c44
Jeon Sangmin <tkdals0766@gmail.com>
2025-04-02 17:55:32 +0900

    boarddto 수정 버전

--- src/main/java/com/multi/semiproject/board/service/BoardService.java
+++ src/main/java/com/multi/semiproject/board/service/BoardService.java
@@ -1,20 +1,20 @@
 package com.multi.semiproject.board.service;
 
 import com.multi.semiproject.board.model.dao.BoardMapper;
+import com.multi.semiproject.board.model.dto.BoardDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
-@RequiredArgsConstructor
-public class BoardService {
-
-    private final BoardMapper boardMapper;
-
-
-    //테스트용
-    public int boardTest() {
-        return boardMapper.boardTest();
-    }
+public interface BoardService {
+    List<BoardDTO> getAllBoards();
+    BoardDTO getBoardById(int id);
+    void insertBoard(BoardDTO board);
+    void updateBoard(BoardDTO board);
+    void deleteBoard(int id);
 }
+
---
+++ src/main/java/com/multi/semiproject/board/service/BoardServiceImpl.java
@@ -0,0 +1,41 @@
+package com.multi.semiproject.board.service;
+
+import com.multi.semiproject.board.model.dao.BoardMapper;
+import com.multi.semiproject.board.model.dto.BoardDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Override
+    public List<BoardDTO> getAllBoards() {
+        return boardMapper.selectAllBoards();
+    }
+
+    @Override
+    public BoardDTO getBoardById(int id) {
+        boardMapper.increaseViewCount(id);
+        return boardMapper.selectBoardById(id);
+    }
+
+    @Override
+    public void insertBoard(BoardDTO board) {
+        boardMapper.insertBoard(board);
+    }
+
+    @Override
+    public void updateBoard(BoardDTO board) {
+        boardMapper.updateBoard(board);
+    }
+
+    @Override
+    public void deleteBoard(int id) {
+        boardMapper.deleteBoard(id);
+    }
+}
